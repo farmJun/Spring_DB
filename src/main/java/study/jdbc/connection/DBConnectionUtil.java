bdc081647bdd1562da7446234f3e96491d8688d0
박준영 <wnsdud6969@naver.com>
2023-06-30 18:19:19 +0900

    feat: 커넥션 구현체를 휙득하는 DBConnectionUtil 클래스를 추가하다

---
+++ src/main/java/study/jdbc/connection/DBConnectionUtil.java
@@ -0,0 +1,22 @@
+package study.jdbc.connection;
+
+import static study.jdbc.connection.ConnectionConst.*;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+import lombok.extern.slf4j.Slf4j;
+
+@Slf4j
+public class DBConnectionUtil {
+	public static Connection getConnection() {
+		try {
+			Connection connection = DriverManager.getConnection(URL, USERNAME, PASSWORD);
+			log.info("get connection={}, class={}", connection, connection.getClass());
+			return connection;
+		} catch (SQLException e) {
+			throw new IllegalArgumentException(e);
+		}
+	}
+}
