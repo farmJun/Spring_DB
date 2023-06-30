e12ca2eac535b39704c893ab3d035063e9709980
박준영 <wnsdud6969@naver.com>
2023-06-30 23:58:07 +0900

    test: HikariCP 커넥션 풀을 사용해 커넥션 휙득을 테스트하다

--- src/test/java/study/jdbc/connection/ConnectionTest.java
+++ src/test/java/study/jdbc/connection/ConnectionTest.java
@@ -11,6 +11,8 @@ import javax.sql.DataSource;
 import org.junit.jupiter.api.Test;
 import org.springframework.jdbc.datasource.DriverManagerDataSource;
 
+import com.zaxxer.hikari.HikariDataSource;
+
 import lombok.extern.slf4j.Slf4j;
 
 @Slf4j
@@ -28,6 +30,19 @@ public class ConnectionTest {
 	void dataSourceDriverManager() throws SQLException {
 		DriverManagerDataSource dataSource = new DriverManagerDataSource(URL, USERNAME, PASSWORD);
 		useDataSource(dataSource);
+	}
+
+	@Test
+	void dataSourceConnectionPool() throws SQLException, InterruptedException {
+		HikariDataSource dataSource = new HikariDataSource();
+		dataSource.setJdbcUrl(URL);
+		dataSource.setUsername(USERNAME);
+		dataSource.setPassword(PASSWORD);
+		dataSource.setMaximumPoolSize(10);
+		dataSource.setPoolName("JunYeong Pool");
+
+		useDataSource(dataSource);
+		Thread.sleep(1000);
 	}
 
 	private void useDataSource(DataSource dataSource) throws SQLException {
