491db8383903f8c7212a4604b869d6ffcdb27346
박준영 <wnsdud6969@naver.com>
2023-06-30 21:59:06 +0900

    feat: 회원 조회를 위한 findById 함수를 추가하다

--- src/main/java/study/jdbc/repository/MemberRepositoryV0.java
+++ src/main/java/study/jdbc/repository/MemberRepositoryV0.java
@@ -5,6 +5,7 @@ import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Statement;
+import java.util.NoSuchElementException;
 
 import lombok.extern.slf4j.Slf4j;
 import study.jdbc.connection.DBConnectionUtil;
@@ -31,6 +32,35 @@ public class MemberRepositoryV0 {
 			throw e;
 		} finally {
 			close(con, pstmt, null);
+		}
+	}
+
+	public Member findById(String memberId) throws SQLException {
+		String sql = "select * from member where member_id = ?";
+
+		Connection con = null;
+		PreparedStatement pstmt = null;
+		ResultSet rs = null;
+
+		try {
+			con = getConnection();
+			pstmt = con.prepareStatement(sql);
+			pstmt.setString(1, memberId);
+			rs = pstmt.executeQuery();
+
+			if (rs.next()) {
+				Member member = new Member();
+				member.setMemberId(rs.getString("member_id"));
+				member.setMoney(rs.getInt("money"));
+				return member;
+			} else {
+				throw new NoSuchElementException("member not found memberId = " + memberId);
+			}
+		} catch (SQLException e) {
+			log.error("db error", e);
+			throw e;
+		} finally {
+			close(con, pstmt, rs);
 		}
 	}
 
