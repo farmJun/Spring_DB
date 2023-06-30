feed60cdf1a0d35c04c7e5061e84d3ff1e73f7b1
박준영 <wnsdud6969@naver.com>
2023-06-30 18:36:25 +0900

    feat: 회원 등록을 위한 save 함수를 추가하다

--- src/main/java/study/jdbc/repository/MemberRepositoryV0.java
+++ src/main/java/study/jdbc/repository/MemberRepositoryV0.java
@@ -1,8 +1,64 @@
 package study.jdbc.repository;
 
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+
 import lombok.extern.slf4j.Slf4j;
+import study.jdbc.connection.DBConnectionUtil;
+import study.jdbc.domain.Member;
 
 @Slf4j
 public class MemberRepositoryV0 {
 
+	public Member save(Member member) throws SQLException {
+		String sql = "insert into member(member_id, money) values (?, ?)";
+
+		Connection con = null;
+		PreparedStatement pstmt = null;
+
+		try {
+			con = getConnection();
+			pstmt = con.prepareStatement(sql);
+			pstmt.setString(1, member.getMemberId());
+			pstmt.setInt(2, member.getMoney());
+			pstmt.executeUpdate();
+			return member;
+		} catch (SQLException e) {
+			log.error("db error", e);
+			throw e;
+		} finally {
+			close(con, pstmt, null);
+		}
+	}
+
+	private void close(Connection con, Statement stmt, ResultSet rs) {
+		if (rs != null) {
+			try {
+				rs.close();
+			} catch (SQLException e) {
+				log.info("error", e);
+			}
+		}
+		if (stmt != null) {
+			try {
+				stmt.close();
+			} catch (SQLException e) {
+				log.info("error", e);
+			}
+		}
+		if (con != null) {
+			try {
+				con.close();
+			} catch (SQLException e) {
+				log.info("error", e);
+			}
+		}
+	}
+
+	private Connection getConnection() {
+		return DBConnectionUtil.getConnection();
+	}
 }
