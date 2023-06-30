9be0447a3840b2f73716f05aebf865dd59132be4
박준영 <wnsdud6969@naver.com>
2023-06-30 23:38:02 +0900

    test: DataSource를 사용해 커넥션 휙득을 테스트하다

--- src/test/java/study/jdbc/connection/ConnectionTest.java
+++ src/test/java/study/jdbc/connection/ConnectionTest.java
@@ -6,7 +6,10 @@ import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
 
+import javax.sql.DataSource;
+
 import org.junit.jupiter.api.Test;
+import org.springframework.jdbc.datasource.DriverManagerDataSource;
 
 import lombok.extern.slf4j.Slf4j;
 
@@ -17,6 +20,19 @@ public class ConnectionTest {
 	void driverManager() throws SQLException {
 		Connection connection1 = DriverManager.getConnection(URL, USERNAME, PASSWORD);
 		Connection connection2 = DriverManager.getConnection(URL, USERNAME, PASSWORD);
+		log.info("connection={}, class={}", connection1, connection1.getClass());
+		log.info("connection={}, class={}", connection2, connection2.getClass());
+	}
+
+	@Test
+	void dataSourceDriverManager() throws SQLException {
+		DriverManagerDataSource dataSource = new DriverManagerDataSource(URL, USERNAME, PASSWORD);
+		useDataSource(dataSource);
+	}
+
+	private void useDataSource(DataSource dataSource) throws SQLException {
+		Connection connection1 = dataSource.getConnection();
+		Connection connection2 = dataSource.getConnection();
 		log.info("connection={}, class={}", connection1, connection1.getClass());
 		log.info("connection={}, class={}", connection2, connection2.getClass());
 	}
