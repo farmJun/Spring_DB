d0b708f0dc6e87eb0363a09d1e187234716a48a8
박준영 <wnsdud6969@naver.com>
2023-06-30 22:07:05 +0900

    test: findById 함수를 테스트하다

--- src/test/java/study/jdbc/repository/MemberRepositoryV0Test.java
+++ src/test/java/study/jdbc/repository/MemberRepositoryV0Test.java
@@ -1,13 +1,15 @@
 package study.jdbc.repository;
 
-import static org.junit.jupiter.api.Assertions.*;
+import static org.assertj.core.api.Assertions.*;
 
 import java.sql.SQLException;
 
 import org.junit.jupiter.api.Test;
 
+import lombok.extern.slf4j.Slf4j;
 import study.jdbc.domain.Member;
 
+@Slf4j
 class MemberRepositoryV0Test {
 
 	MemberRepositoryV0 repository = new MemberRepositoryV0();
@@ -16,5 +18,16 @@ class MemberRepositoryV0Test {
 	void save() throws SQLException {
 		Member member = new Member("memberV0", 10000);
 		repository.save(member);
+	}
+
+	@Test
+	void crud() throws SQLException {
+		Member member = new Member("memberV0", 10000);
+		repository.save(member);
+
+		Member findMember = repository.findById(member.getMemberId());
+		log.info("findMember={}", findMember);
+
+		assertThat(findMember).isEqualTo(member);
 	}
 }
