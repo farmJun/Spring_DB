ec6041ffb17716c76adc5168228a58f5ac7b6b45
박준영 <wnsdud6969@naver.com>
2023-07-24 18:57:42 +0900

    test: 자바 체크 예외 테스트

---
+++ src/test/java/study/jdbc/exception/basic/CheckedTest.java
@@ -0,0 +1,50 @@
+package study.jdbc.exception.basic;
+
+import lombok.extern.slf4j.Slf4j;
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
+
+@Slf4j
+public class CheckedTest {
+    @Test
+    void checked_catch() {
+        Service service = new Service();
+        service.callCatch();
+    }
+
+    @Test
+    void checked_throw() {
+        Service service = new Service();
+        assertThatThrownBy(() -> service.callThrow()).isInstanceOf(MyCheckedException.class);
+    }
+
+    static class MyCheckedException extends Exception {
+        public MyCheckedException(String message) {
+            super(message);
+        }
+    }
+
+    static class Service {
+        Repository repository = new Repository();
+
+        public void callCatch() {
+            try {
+                repository.call();
+            } catch (MyCheckedException e) {
+                //예외 처리 로직
+                log.info("예외 처리,message={}", e.getMessage(), e);
+            }
+        }
+
+        public void callThrow() throws MyCheckedException {
+            repository.call();
+        }
+    }
+
+    static class Repository {
+        public void call() throws MyCheckedException {
+            throw new MyCheckedException("ex");
+        }
+    }
+}
