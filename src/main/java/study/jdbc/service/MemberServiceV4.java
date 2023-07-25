2b93ac70e73a2802cad482f29c0b5435405ff55a
박준영 <wnsdud6969@naver.com>
2023-07-25 12:36:23 +0900

    feat: MemberServiceV4 구현

---
+++ src/main/java/study/jdbc/service/MemberServiceV4.java
@@ -0,0 +1,32 @@
+package study.jdbc.service;
+
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.transaction.annotation.Transactional;
+import study.jdbc.domain.Member;
+import study.jdbc.repository.MemberRepository;
+
+@Slf4j
+@RequiredArgsConstructor
+public class MemberServiceV4 {
+    private final MemberRepository memberRepository;
+
+    @Transactional
+    public void accountTransfer(String fromId, String toId, int money) {
+        bizLogic(fromId, toId, money);
+    }
+
+    private void bizLogic(String fromId, String toId, int money) {
+        Member fromMember = memberRepository.findById(fromId);
+        Member toMember = memberRepository.findById(toId);
+        memberRepository.update(fromId, fromMember.getMoney() - money);
+        validation(toMember);
+        memberRepository.update(toId, toMember.getMoney() + money);
+    }
+
+    private void validation(Member toMember) {
+        if (toMember.getMemberId().equals("ex")) {
+            throw new IllegalStateException("이체중 예외 발생");
+        }
+    }
+}
