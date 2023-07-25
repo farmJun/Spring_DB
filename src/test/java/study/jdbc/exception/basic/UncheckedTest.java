63f1474458c22a9ede5e7ac3616e0a4902558143
박준영 <wnsdud6969@naver.com>
2023-07-25 23:11:23 +0900

    test: 자바 언체크 예외 테스트

---
+++ src/test/java/study/jdbc/exception/basic/UncheckedTest.java
@@ -0,0 +1,50 @@
+package study.jdbc.exception.basic;
+
+import lombok.extern.slf4j.Slf4j;
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
+
+@Slf4j
+public class UncheckedTest {
+    @Test
+    void unchecked_catch() {
+        Service service = new Service();
+        service.callCatch();
+    }
+
+    @Test
+    void unchecked_throw() {
+        Service service = new Service();
+        assertThatThrownBy(() -> service.callThrow()).isInstanceOf(MyUncheckedException.class);
+    }
+
+    static class MyUncheckedException extends RuntimeException {
+        public MyUncheckedException(String message) {
+            super(message);
+        }
+    }
+
+    static class Service {
+        Repository repository = new Repository();
+
+        public void callCatch() {
+            try {
+                repository.call();
+            } catch (MyUncheckedException e) {
+                //예외 처리 로직
+                log.info("예외 처리,message={}", e.getMessage(), e);
+            }
+        }
+
+        public void callThrow() {
+            repository.call();
+        }
+    }
+
+    static class Repository {
+        public void call() {
+            throw new MyUncheckedException("ex");
+        }
+    }
+}
