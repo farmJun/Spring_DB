4f9288b7415e715d14b306545b5c766e732a1587
박준영 <wnsdud6969@naver.com>
2023-08-04 19:27:23 +0900

    test: 스프링 제공 예외 변환기 테스트

---
+++ src/test/java/study/jdbc/exception/translator/SpringExceptionTranslatorTest.java
@@ -0,0 +1,41 @@
+package study.jdbc.exception.translator;
+
+import lombok.extern.slf4j.Slf4j;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.springframework.jdbc.datasource.DriverManagerDataSource;
+
+import javax.sql.DataSource;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static study.jdbc.connection.ConnectionConst.*;
+
+@Slf4j
+public class SpringExceptionTranslatorTest {
+    DataSource dataSource;
+
+    @BeforeEach
+    void init() {
+        dataSource = new DriverManagerDataSource(URL, USERNAME, PASSWORD);
+    }
+
+    @Test
+    void sqlExceptionErrorCode() {
+        String sql = "select bad grammar";
+        try {
+            Connection con = dataSource.getConnection();
+            PreparedStatement stmt = con.prepareStatement(sql);
+            stmt.executeQuery();
+        } catch (SQLException e) {
+            assertThat(e.getErrorCode()).isEqualTo(42122);
+            int errorCode = e.getErrorCode();
+            log.info("errorCode={}", errorCode);
+            //org.h2.jdbc.JdbcSQLSyntaxErrorException
+            log.info("error", e);
+        }
+    }
+}
